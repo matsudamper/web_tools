bba14f662827c177fc086373fde31f551aadd0af
matsudamper <let@matsudamper.net>
2022-10-28 04:26:01 +0900

    テキストの編集を実装

--- src/jsMain/kotlin/screen/RootScreenViewModel.kt
+++ src/jsMain/kotlin/screen/RootScreenViewModel.kt
@@ -1,96 +1,73 @@
 package screen
 
-import com.npmjs.UuidJs
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import net.matsudamper.tools.web.repository.query.QueryRepository
+import net.matsudamper.tools.web.repository.url.UrlRepository
 
 class RootScreenViewModel(
     private val coroutineScope: CoroutineScope,
 ) {
+    private val urlRepository = UrlRepository()
+    private val queryRepository = QueryRepository()
+
     private val _uiStateFlow: MutableStateFlow<RootScreenUiState> = MutableStateFlow(
         RootScreenUiState(
             panelsUiStates = listOf(),
             onClickAdd = {
-                addUrl()
+                urlRepository.insert()
             },
         )
     )
-
-    private fun addUrl() {
-        viewModelStateFlow.update { viewModelState ->
-            val usedUUIDs = viewModelState.urlEditPanelUiStates.map { it.uuid }
-            val uuid = UuidJs.generate { it !in usedUUIDs }
-            viewModelState.copy(
-                urlEditPanelUiStates = viewModelStateFlow.value.urlEditPanelUiStates.plus(
-                    UrlEditPanelVMState(
-                        uuid = uuid,
-                        uiState = UrlEditPanelUiState(
-                            name = viewModelStateFlow.value.urlEditPanelUiStates.size.toString(),
-                            base = "",
-                            queries = listOf(),
-                            onClickAddQuery = {
-                                addQuery(uuid = uuid)
-                            }
-                        )
-                    )
-                )
-            )
-        }
-    }
 
     val uiStateFlow: StateFlow<RootScreenUiState> = _uiStateFlow.asStateFlow()
     private val viewModelStateFlow: MutableStateFlow<VMState> = MutableStateFlow(VMState())
 
     init {
         coroutineScope.launch {
-            viewModelStateFlow.collect { viewModelState ->
+            combine(
+                flows = arrayListOf(
+                    urlRepository.getAll(),
+                    queryRepository.getAll(),
+                )
+            ) {
                 _uiStateFlow.value = _uiStateFlow.value.copy(
-                    panelsUiStates = viewModelState.urlEditPanelUiStates.map { it.uiState },
-                )
-            }
-        }
-    }
-
-    private fun addQuery(uuid: String) {
-        viewModelStateFlow.update { viewModelState ->
-            val targetIndex = viewModelState.urlEditPanelUiStates.indexOfFirst { it.uuid == uuid }
-                .takeIf { it >= 0 }
-                ?: return
-
-            val target = viewModelState.urlEditPanelUiStates[targetIndex]
-
-            val newState = target.copy(
-                uiState = target.uiState.copy(
-                    queries = target.uiState.queries.plus(
-                        UrlEditPanelUiState.Query(
-                            key = "",
-                            value = "",
-                            onKeyChange = {},
-                            onValueChange = {},
+                    panelsUiStates = urlRepository.getAll().value.map { urlModel ->
+                        UrlEditPanelUiState(
+                            name = urlModel.name,
+                            base = urlModel.base,
+                            queries = queryRepository.get(urlModel.uuid).map { queryModel ->
+                                UrlEditPanelUiState.Query(
+                                    key = queryModel.key,
+                                    value = queryModel.value,
+                                    onKeyChange = {
+                                        queryRepository.update(
+                                            queryModel.copy(
+                                                key = it
+                                            )
+                                        )
+                                    },
+                                    onValueChange = {
+                                        queryRepository.update(
+                                            queryModel.copy(
+                                                value = it
+                                            )
+                                        )
+                                    },
+                                )
+                            },
+                            onClickAddQuery = {
+                                queryRepository.insert(urlModel.uuid)
+                            },
                         )
-                    )
+                    },
                 )
-            )
-
-            viewModelState.copy(
-                urlEditPanelUiStates = viewModelState.urlEditPanelUiStates.toMutableList().also {
-                    it.removeAt(targetIndex)
-                    it.add(targetIndex, newState)
-                }
-            )
+            }.collect()
         }
     }
 
     data class VMState(
-        val urlEditPanelUiStates: List<UrlEditPanelVMState> = listOf()
-    )
-
-    data class UrlEditPanelVMState(
-        val uuid: String,
-        val uiState: UrlEditPanelUiState,
+        val none: Unit = Unit
     )
 }
