17659cd1f1f6cf7ae1fce64a3e3ddb103224cfb1
matsudamper <let@matsudamper.net>
2022-11-16 02:40:09 +0900

    root画面を作成

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,13 +1,24 @@
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import kotlinx.browser.document
+import net.matsudamper.tools.web.compose.component.FlexRow
+import net.matsudamper.tools.web.compose.component.Scaffold
+import net.matsudamper.tools.web.compose.component.ToggleButton
+import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposableInBody
 import org.w3c.dom.HTMLBodyElement
 import org.w3c.dom.HTMLHtmlElement
 import org.w3c.dom.get
-import screen.RootScreen
+import screen.DynamicLinkBuilderScreen
 import screen.RootScreenViewModel
+import net.matsudamper.tools.web.compose.root.RootScreen
+import net.matsudamper.tools.web.compose.theme.LocalContentColor
+import net.matsudamper.tools.web.compose.theme.LocalSurfaceColor
+import net.matsudamper.tools.web.compose.theme.Theme
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Input
+import screen.player.PlayerScreen
 
 fun main() {
     run {
@@ -24,13 +35,62 @@ fun main() {
     }
 
     renderComposableInBody {
-        val coroutineScope = rememberCoroutineScope()
-        val viewModel = remember(coroutineScope) {
-            RootScreenViewModel(coroutineScope = coroutineScope)
-        }
+        var isDarkMode by remember { mutableStateOf(true) }
+        Theme(isDarkMode = isDarkMode) {
+            Scaffold(
+                topAppBar = {
+                    FlexRow(
+                        attrs = {
+                            style {
+                                height(50.px)
+                                width(100.percent)
+                                backgroundColor(Color("#3c4043"))
+                                property("box-shadow", "1px 1px transparent")
+                            }
+                        },
+                        alignContent = AlignContent.Center,
+                    ) {
+                        Text("Web Tools")
+                        Div({ style { flexGrow(1) } }) { }
+                        Text("isDarkMode")
+                        ToggleButton(
+                            value = isDarkMode,
+                            onChanged = {
+                                isDarkMode = it
+                            }
+                        )
+                    }
+                }
+            ) {
+                when (document.location!!.pathname) {
+                    "/player/" -> {
+                        LaunchedEffect(Unit) {
+                            isDarkMode = true
+                        }
+                        PlayerScreen()
+                    }
 
-        RootScreen(
-            uiState = viewModel.uiStateFlow.collectAsState().value
-        )
+                    "/dynamic_link_builder/" -> {
+                        LaunchedEffect(Unit) {
+                            isDarkMode = false
+                        }
+                        val coroutineScope = rememberCoroutineScope()
+                        val viewModel = remember(coroutineScope) {
+                            RootScreenViewModel(coroutineScope = coroutineScope)
+                        }
+                        DynamicLinkBuilderScreen(
+                            uiState = viewModel.uiStateFlow.collectAsState().value
+                        )
+                    }
+
+                    else -> {
+                        LaunchedEffect(Unit) {
+                            isDarkMode = false
+                        }
+                        RootScreen()
+                    }
+                }
+            }
+        }
     }
 }
